740cc04f5c759a57a88be73003473e53e9aa7fb5
Galamou <males97@live.com>
2020-04-12 16:36:45 +0200

    Routing added for profile and sign-up

--- AeroCar/src/app/app-routing.module.ts
+++ AeroCar/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
