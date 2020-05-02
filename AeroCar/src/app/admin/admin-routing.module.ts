75706374e201f7b39eaa109783d04a0ea870dad3
Galamou <males97@live.com>
2020-05-03 01:24:47 +0200

    Added car admin component and a few changes

--- AeroCar/src/app/admin/admin-routing.module.ts
+++ AeroCar/src/app/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SystemComponent } from './system/system.component';
 import { AdminComponent } from './admin.component';
+import { CarComponent } from './car/car.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
-  { path: 'system', component: SystemComponent }
+  { path: 'system', component: SystemComponent },
+  { path: 'car', component: CarComponent }
 ]
 
 @NgModule({
--- AeroCar/src/app/admin/admin.module.ts
+++ AeroCar/src/app/admin/admin.module.ts
@@ -4,11 +4,12 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { SystemComponent } from './system/system.component';
+import { CarComponent } from './car/car.component';
 
 
 
 @NgModule({
-  declarations: [AdminComponent, SystemComponent],
+  declarations: [AdminComponent, SystemComponent, CarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
