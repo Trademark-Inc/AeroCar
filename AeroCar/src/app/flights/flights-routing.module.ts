7cef90d2e6330bc465b9ed9dd6c8e168f8b2d807
tamaramarkovic <tmtamaramarkovic@gmail.com>
2020-05-12 17:56:27 +0200

    FlightReservation component added

--- AeroCar/src/app/flights/flights-routing.module.ts
+++ AeroCar/src/app/flights/flights-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FlightsComponent } from './flights.component';
+import { FlightReservationComponent } from './reservation/reservation.component';
 
 const routes: Routes = [
-    { path: '', component: FlightsComponent, data: { animation: 'isRight' } }
+    { path: '', component: FlightsComponent, data: { animation: 'isRight' } },
+    { path: 'reservation', component: FlightReservationComponent}
 ]
 
 @NgModule({
