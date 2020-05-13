251db53b9b541e92bd50f3e359e69788241dc8e6
tamaramarkovic <tmtamaramarkovic@gmail.com>
2020-05-13 02:18:45 +0200

    Update reservation

--- AeroCar/src/app/flights/flights-routing.module.ts
+++ AeroCar/src/app/flights/flights-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FlightsComponent } from './flights.component';
 import { FlightReservationComponent } from './reservation/reservation.component';
+import { FlightSeatsComponent } from './reservation/seats/seats.component';
+import { FlightDetailsComponent } from './reservation/seats/details/details.component';
 
 const routes: Routes = [
     { path: '', component: FlightsComponent, data: { animation: 'isRight' } },
-    { path: 'reservation', component: FlightReservationComponent}
+    { path: 'reservation', component: FlightReservationComponent},
+    { path: 'reservation/seats', component: FlightSeatsComponent},
+    { path: 'reservation/seats/details', component: FlightDetailsComponent}
 ]
 
 @NgModule({
--- AeroCar/src/app/flights/flights.module.ts
+++ AeroCar/src/app/flights/flights.module.ts
@@ -4,10 +4,14 @@ import { FlightsRoutingModule } from './flights-routing.module';
 
 import { FlightsComponent } from './flights.component';
 import { FlightReservationComponent } from './reservation/reservation.component';
+import { FlightSeatsComponent } from './reservation/seats/seats.component';
+import { FlightDetailsComponent } from './reservation/seats/details/details.component';
 
 @NgModule({
   declarations: [FlightsComponent, 
-                 FlightReservationComponent],
+                 FlightReservationComponent,
+                 FlightSeatsComponent,
+                 FlightDetailsComponent],
   imports: [
     CommonModule,
     FlightsRoutingModule
