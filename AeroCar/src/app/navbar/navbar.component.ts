a7ee97f9927cdae0a38cf6a7b77b5db1c3925b16
Galamou <males97@live.com>
2020-05-23 20:23:46 +0200

    Login with backend completed

--- AeroCar/src/app/navbar/navbar.component.ts
+++ AeroCar/src/app/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'app-navbar',
@@ -7,13 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean;
+  public failed: boolean;
+
+  constructor(public http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  signIn(): void {
-    
+  signIn(form: NgForm): void {
+    console.log(form);
+    var jsonized = JSON.stringify(form.value);
+    console.log(jsonized);
+    this.loading = true;
+    this.failed = false;
+    var ret = this.http.post("http://localhost:62541/api/user/login", jsonized, { 
+      headers: {'Content-Type': 'application/json'},
+      observe: 'response',
+      responseType: 'json' }).subscribe(data => {
+        console.log("DATA");
+        console.log(data);
+        this.loading = false;
+        this.router.navigateByUrl('/profile');
+      },
+      err => {
+        console.log("ERROR");
+        console.log(err);
+        this.loading = false;
+        this.failed = true;
+      });
+    console.log(ret);
   }
 
 }
