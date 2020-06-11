29e090d4b519d655a623a45f8b0dd8d0434d509f
tamaramarkovic <tmtamaramarkovic@gmail.com>
2020-06-11 17:46:58 +0200

    Avio company profile details

--- AeroCar/src/app/flights/profile/profile.component.ts
+++ AeroCar/src/app/flights/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightsProfileComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  profileDetails: any;
+
+  constructor(private route: ActivatedRoute, public http: HttpClient, private router: Router, private zone: NgZone) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.loadProfileData();
+   }
 
   ngOnInit(): void {
+  }
+
+  loadProfileData(): void {
+    var ret = this.http.get("http://localhost:62541/api/avio/company/details/get/" + this.id, {
+      headers: {'Content-Type': 'application/json'},
+      observe: 'response',
+      withCredentials: true,
+      responseType: 'json' }).subscribe(data => {
+        console.log("DATA");
+        console.log(data);
+        console.log(data.body);
+        this.zone.run(() => { 
+          this.profileDetails = data.body["avioCompanyProfileDTO"]
+      });
+      },
+      err => {
+        console.log("ERROR");
+        console.log(err);
+      });
   }
 
 }
